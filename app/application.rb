da94abde906abb92a6ebdb7016766b1f9879fc3b
Nicholas Ng <nicholasykng@gmail.com>
2019-12-11 03:19:35 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,12 @@
+class Application
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+    if req.path=="/items"
+      resp.status = 200
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+  end
+end
